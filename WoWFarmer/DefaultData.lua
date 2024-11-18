0d24a0707e43c6d17780bb917c4450f0532e15e3
rdstickle <rdstickle2021@gmail.com>
2024-11-18 14:59:08 -0700

    Create DefaultData.lua

---
+++ WoWFarmer/DefaultData.lua
@@ -0,0 +1,32 @@
+-- WoW Farmer Default Data Lua File
+-- This file contains pre-populated drop rates and item information for cloth and ore.
+
+DefaultData = {
+    items = {
+        -- Cloth Items
+        [2589] = { name = "Linen Cloth", defaultDropRate = 10 },        -- Linen Cloth
+        [2592] = { name = "Wool Cloth", defaultDropRate = 10 },         -- Wool Cloth
+        [4306] = { name = "Silk Cloth", defaultDropRate = 10 },         -- Silk Cloth
+        [4338] = { name = "Mageweave Cloth", defaultDropRate = 10 },    -- Mageweave Cloth
+        [14047] = { name = "Runecloth", defaultDropRate = 10 },         -- Runecloth
+        [14256] = { name = "Felcloth", defaultDropRate = 10 },          -- Felcloth
+        [14342] = { name = "Mooncloth", defaultDropRate = 10 },         -- Mooncloth
+        [21877] = { name = "Netherweave Cloth", defaultDropRate = 10 }, -- Netherweave Cloth
+        [33470] = { name = "Frostweave Cloth", defaultDropRate = 10 },  -- Frostweave Cloth
+        [53010] = { name = "Embersilk Cloth", defaultDropRate = 10 },   -- Embersilk Cloth
+
+        -- Ore Items
+        [2770] = { name = "Copper Ore", defaultDropRate = 10 },         -- Copper Ore
+        [2771] = { name = "Tin Ore", defaultDropRate = 10 },            -- Tin Ore
+        [2772] = { name = "Iron Ore", defaultDropRate = 10 },           -- Iron Ore
+        [3858] = { name = "Mithril Ore", defaultDropRate = 10 },        -- Mithril Ore
+        [10620] = { name = "Thorium Ore", defaultDropRate = 10 },       -- Thorium Ore
+        [23424] = { name = "Fel Iron Ore", defaultDropRate = 10 },      -- Fel Iron Ore
+        [23425] = { name = "Adamantite Ore", defaultDropRate = 10 },    -- Adamantite Ore
+        [36909] = { name = "Cobalt Ore", defaultDropRate = 10 },        -- Cobalt Ore
+        [36912] = { name = "Saronite Ore", defaultDropRate = 10 },      -- Saronite Ore
+        [52183] = { name = "Pyrite Ore", defaultDropRate = 10 },        -- Pyrite Ore
+        [53038] = { name = "Obsidium Ore", defaultDropRate = 10 },      -- Obsidium Ore
+        [52185] = { name = "Elementium Ore", defaultDropRate = 10 },    -- Elementium Ore
+    }
+}
